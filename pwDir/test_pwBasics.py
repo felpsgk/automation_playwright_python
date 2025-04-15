a28d0ac2dbc184f137e3f0432e76e4105fff9c76
Felipe <39858416+felpsgk@users.noreply.github.com>
2025-04-15 07:53:01 -0300

    path full

--- pwDir/test_pwBasics.py
+++ pwDir/test_pwBasics.py
@@ -22,15 +22,15 @@ def test_login():
     with iniciar_browser() as (page, browser):
         try:
             navegarPara(page, "https://apphml.unimedbh.com.br/unimedagenda")
-            printTela(page, "link.png")  # Captura um print inicial
+            printTela(page, "C:\ProgramData\Jenkins\slave\workspace\python\link.png")  # Captura um print inicial
             preencherCampo(page, "#username", "ua_automasuper")
-            printTela(page, "login.png")  # Captura um print inicial
+            printTela(page, "C:\ProgramData\Jenkins\slave\workspace\python\login.png")  # Captura um print inicial
             preencherCampo(page, "#password", "Ab147258369@")
             clicarBotaoPorTexto(page, "Entrar")
-            printTela(page, "entrar.png")  # Captura um print inicial
+            printTela(page, "C:\ProgramData\Jenkins\slave\workspace\python\entrar.png")  # Captura um print inicial
             esperaPorTexto(page, "Parabéns, seus dados foram validados com sucesso", 7000)
-            printTela(page, "sucesso.png")  # Captura um print inicial
+            printTela(page, "C:\ProgramData\Jenkins\slave\workspace\python\sucesso.png")  # Captura um print inicial
         except Exception as e:
             # Captura um print em caso de erro
-            page.screenshot(path="screenshot_error.png")
+            page.screenshot(path="C:\ProgramData\Jenkins\slave\workspace\python\screenshot_error.png")
             raise e  # Relança o erro após salvar o print
